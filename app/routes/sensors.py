00b4072cbf4ac5517e921210983916d59e0049e4
santiago-reto <santiago@reto.design>
2025-09-14 16:30:41 -0600

    Estado manual bomba

--- app/routes/sensors.py
+++ app/routes/sensors.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from ..extensions import db
 from ..models import EstadoBomba
+from datetime import datetime
 
 sensors_bp = Blueprint("sensors", __name__)
 
@@ -9,12 +10,37 @@ def add_pump_data():
     data = request.get_json()
     if not data or "estado" not in data or "tiempo_llenado" not in data:
         return jsonify({"error": "Faltan datos"}), 400
+    
+    estado = data["estado"]
+    tiempo_llenado = data["tiempo_llenado"]
+
+    # Si el estado es "Apagado manual", calcular tiempo de llenado desde el último "Encendido manual"
+    if estado == "Apagado manual":
+        ultimo_encendido = EstadoBomba.query.order_by(EstadoBomba.fecha_creacion.desc()).first()
+        if ultimo_encendido == 'Prendido manual':
+            tiempo_llenado = (datetime.now() - ultimo_encendido.fecha_creacion).total_seconds()
 
     new_entry = EstadoBomba(
-        estado=data["estado"],
-        tiempo_llenado=data["tiempo_llenado"]
+        estado=estado,
+        tiempo_llenado=tiempo_llenado
     )
     db.session.add(new_entry)
     db.session.commit()
 
     return jsonify({"message": "Datos guardados correctamente", "id": new_entry.id}), 201
+
+@sensors_bp.route("/last_pump_state", methods=["GET"])
+def last_pump_state():
+    # Consulta el último registro ordenando por fecha de creación descendente
+    last_entry = EstadoBomba.query.order_by(EstadoBomba.fecha_creacion.desc()).first()
+    
+    if not last_entry:
+        return jsonify({"error": "No hay registros"}), 404
+
+    # Retorna los datos en formato JSON
+    return jsonify({
+        "id": last_entry.id,
+        "estado": last_entry.estado,
+        "tiempo_llenado": last_entry.tiempo_llenado,
+        "fecha_creacion": last_entry.fecha_creacion
+    }), 200
