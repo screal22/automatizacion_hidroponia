dc7a4eb4862fdafbab976000f1cdf57ba178b757
santiago-reto <santiago@reto.design>
2025-09-21 01:13:14 -0600

    Actualización logger y conexión BD

--- app/mqtt_listener.py
+++ app/mqtt_listener.py
@@ -3,13 +3,9 @@ from app.extensions import db
 from app.models import EstadoBomba
 from dotenv import load_dotenv
 import os
-import logging
 
 # Carga de variables de entorno
 load_dotenv()
-
-# Configuración del logging
-logging.basicConfig(level=logging.INFO)
 
 # Configuración de MQTT
 MQTT_BROKER = os.getenv("MQTT_BROKER", "test.mosquitto.org")
@@ -18,20 +14,16 @@ MQTT_TOPIC = os.getenv("MQTT_TOPIC", 'hidroponia/valvula/estado')
 print('Configuración MQTT:', MQTT_BROKER, MQTT_PORT, MQTT_TOPIC)
 
 def start_mqtt_listener(app):
-    client = mqtt.Client()
-    app.logger.info('Configuración MQTT:', MQTT_BROKER, MQTT_PORT, MQTT_TOPIC)
-    logging.info(f'Configuración MQTT: {MQTT_BROKER}, {MQTT_PORT}, {MQTT_TOPIC}')
+    app.logger.info(f'Configuración MQTT: {MQTT_BROKER}, {MQTT_PORT}, {MQTT_TOPIC}')
 
     def on_connect(client, userdata, flags, rc):
         print("Conectado al broker MQTT con código:", rc)
-        logging.info(f"Conectado al broker MQTT con código: {rc}")
         client.subscribe(MQTT_TOPIC)
 
     def on_message(client, userdata, msg):
         msg_decodificado = msg.payload.decode("utf-8").split('/')
         print(f"Mensaje recibido en {msg.topic}: {msg_decodificado}")
         app.logger.info(f"Mensaje recibido en {msg.topic}: {msg_decodificado}")
-        logging.info(f"Mensaje recibido en {msg.topic}: {msg_decodificado}")
 
         # Partimos el mensaje en estado y tiempo_llenado
         estado = msg_decodificado[0]
@@ -41,11 +33,12 @@ def start_mqtt_listener(app):
             estado=estado,
             tiempo_llenado=tiempo_llenado
         )
-        db.session.add(nuevo_registro)
-        db.session.commit()
+        with app.app_context():
+            db.session.add(nuevo_registro)
+            db.session.commit()
+
         print(f"Estado {estado} guardado en la base de datos")
         app.logger.info(f"Estado {estado} guardado en la base de datos")
-        logging.info(f"Estado {estado} guardado en la base de datos")
 
     client = mqtt.Client()
     client.on_connect = on_connect
